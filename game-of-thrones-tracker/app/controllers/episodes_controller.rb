ff420860240a2bc792f7022b5a1f4023afbfd870
jensnyder <jlsnyder09@gmail.com>
2018-08-24 16:10:43 -0400

    final adjustments

--- game-of-thrones-tracker/app/controllers/episodes_controller.rb
+++ game-of-thrones-tracker/app/controllers/episodes_controller.rb
@@ -2,7 +2,9 @@ class EpisodesController < ApplicationController
 
   def index
     @episodes = Episode.all
-    @user = User.find(session[:user_id])
+    if session[:user_id]
+      @user = User.find(session[:user_id])
+    end
   end
 
   def show
